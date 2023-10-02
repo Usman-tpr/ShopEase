ee8a6ad72eb3f46f0b0a6638200ea1a91102fa7d
Usman Ali <114804406+Usman-tpr@users.noreply.github.com>
2023-10-02 23:04:54 +0500

    carasule

---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {BiSolidUserCircle , BiSolidLogInCircle , BiSolidCart} from 'react-icons/bi'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+    
+       <div className="container-xl">
+            <div className="row p-3 d-flex align-items-center justify-content-between">
+                <div className="col-sm-2"> 
+                <h3 className='fw-bold text-white'>shopEase</h3>
+                </div>
+                <div className="col-sm-5 d-flex"> 
+                <input type="text" className='form-control' placeholder='Search Here' />
+                <a className='bg-custom btn text-white'>Search</a>
+                </div>
+
+                <div className="col-sm-4 d-flex gap-1 align-items-center justify-content-around nav-items">
+                <div><BiSolidUserCircle /> <a href="#" > Login</a></div>
+            <div>    <BiSolidLogInCircle  />    <a href="#" >Signup</a></div>
+                <div><BiSolidCart />    <a href="#">cart</a></div>
+                </div>
+            </div>
+       </div>
+    </div>
+
+  )
+}
+
+export default Header
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 const Home = () => {
+  const picsArray = ['/assets/images/landscape.jpg','/assets/images/download.jpeg','/assets/images/sunset.jpg'];
+  const [changeImage,setChangeImage] = useState(0);
+    
+const handleChange = () =>{
+       if(changeImage>2){
+        setChangeImage(0);
+       }
+       const index = changeImage+1;
+      
+       console.log(changeImage)
+     console.log('testing')
+     setTimeout(()=>{
+      
+      setChangeImage(index)
+     },[3000])
+  }
+// handleChange()
+
+
   return (
-    <div>Home</div>
+  <>
+        <div className='container'>
+          <div className="row">
+            <div className="col-sm-6">
+              <img src={picsArray[changeImage]} alt="" />
+            </div>
+          </div>
+
+        </div>
+  </>
   )
 }
 
